611c3976faf8fd9f2c6b4bd5495a46af7ae5e88b
Baptiste Dumortier <baptiste@dumortier.design>
2018-10-02 22:58:39 +0200

    push it

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'My first test',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site. ça marche tout seul je pense </p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Voir video</Link>
+  </div>
+)
+
+export default IndexPage
